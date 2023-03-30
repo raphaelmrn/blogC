465e925ed34f427bd0a5be8f17fa5c1e3421b0e0
Raphael Marion <raphaelmarion@mbp-de-raphael.home>
2023-03-30 10:06:36 +0200

    page with layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,11 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
+  ],
+
+  theme: {
+      fontFamily: {
+        'sans': ['Oswald']
+      },
+    }
 }
